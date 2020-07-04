a179629cd0494823421df7eb88e2c924121b158b
lichitw2 <64572240+lichitw2@users.noreply.github.com>
2020-07-05 01:07:25 +0800

    Update 02_target_crawler.py

--- 02_target_crawler.py
+++ 02_target_crawler.py
@@ -4,27 +4,38 @@ import json
 import time
 import pandas as pd
 import re
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 
-df = pd.read_csv('keywords.csv')
+df = pd.read_csv('keywords_NY2.csv')
 
 Row_list =[] 
 for index, row in df.iterrows(): 
     my_list =[row.product_name, row.aisle] 
     Row_list.append(my_list) 
 
-product_info = {}
 product_result = []
 
-for row in Row_list[1:2]:
+for row in Row_list:
     if type(row[1]) != str:
         keywords = row[0]
     else:
         keywords = row[0]+" "+ row[1]
     print(Row_list.index(row),keywords)
 
-    url = "https://redsky.target.com/v2/plp/search/?channel=web&count=6&default_purchasability_filter=true&facet_recovery=false&fulfillment_test_mode=grocery_opu_team_member_test&isDLP=false&keyword={}&offset=0&pageId=%2Fs%2F{}&pricing_store_id=1357&store_ids=1357&visitorId=01731365EC690201848CEBB8FAEBCD10&include_sponsored_search_v2=false&ppatok=AOxT33a&platform=desktop&useragent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F83.0.4103.116+Safari%2F537.36&excludes=available_to_promise_qualitative%2Cavailable_to_promise_location_qualitative&key=eb2551e4accc14f38cc42d32fbc2b2ea".format(keywords,keywords)
+    url = 'https://redsky.target.com/v2/plp/search/?channel=web&count=24&default_purchasability_filter=true&facet_recovery=false&fulfillment_test_mode=grocery_opu_team_member_test&isDLP=false&keyword=Peanut+Butter+Cereal&offset=0&pageId=%2Fs%2FPeanut+Butter+Cereal&pricing_store_id=1306&store_ids=1306%2C3294%2C198%2C2775%2C2632&visitorId=0172C7C1A02802019D4E379FB6434C3C&include_sponsored_search_v2=true&ppatok=AOxT33a&platform=mobile&useragent=Mozilla%2F5.0+%28Linux%3B+Android+6.0%3B+Nexus+5+Build%2FMRA58N%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F83.0.4103.116+Mobile+Safari%2F537.36&excludes=available_to_promise_qualitative%2Cavailable_to_promise_location_qualitative&key=eb2551e4accc14f38cc42d32fbc2b2ea'
     headers = {"User-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
-
+    
+    url = urlparse(url)
+    list_url = list(url)
+    #set new params
+    params = dict( (k, v if len(v)>1 else v[0] ) for k, v in parse_qs(url.query).items() )
+    params['include_sponsored_search_v2'] = 'false'
+    params['keyword'] = keywords
+    params['pageId'] = '/s/%s'%(keywords)
+    
+    list_url[4] = urlencode(params)
+    url = urlunparse(list_url)
+    
     res = requests.get(url, headers = headers)
 
     json_res = json.loads(res.text)
@@ -32,6 +43,7 @@ for row in Row_list[1:2]:
     
 
     for data in json_data:
+        product_info = {}
         product_info['keyword'] = row[0] # 商品關鍵字
         
         if 'title' in data.keys():
@@ -44,13 +56,14 @@ for row in Row_list[1:2]:
             product_info['pic'] = data["images"][0]["base_url"] + data["images"][0]["primary"]# 商品圖片
             
         if "description" in data.keys():
-            product_info['Description'] = data["description"].replace('<br /><br />', ' ') # 商品描述
+            product_info['Description'] = data["description"].replace('<br />', ' ') # 商品描述 #空白敘述處理
             
         if "brand" in data.keys():
             product_info['brand'] = data["brand"] # 商品品牌
             
         if "average_rating" in data.keys():
             product_info['star_ratings'] = [data["average_rating"]] # 平均評分
+            # product_info['star_ratings'] = [product_ratings]
             
         if "total_reviews" in data.keys():
             product_info['star_ratings'].append(data["total_reviews"]) # 評分人數
@@ -96,11 +109,9 @@ for row in Row_list[1:2]:
         product_cats = soup_cat.select('span[itemprop]')
         product_info['category'] = [x.text for x in product_cats]
 
-        #product_result = json.dumps(product_info, ensure_ascii = False)
-
-        print(product_info)
         product_result.append(product_info)
-        print("============")
-        
-with open('product_info.json', 'w') as outfile:
-    json.dump(product_result, outfile,ensure_ascii=False, indent=2)
+        print(product_result)
+        print("===========")
+
+        with open('product_info.json', 'w',encoding = 'utf-8') as outfile:
+            json.dump(product_result, outfile, ensure_ascii = False, indent = 4)
