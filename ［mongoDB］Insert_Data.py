35e8f6c9906ae0f42f7ef59f90fb5db0055d3562
lichitw2 <64572240+lichitw2@users.noreply.github.com>
2020-07-07 11:50:21 +0800

    Update ［mongoDB］Insert_Data.py

--- ［mongoDB］Insert_Data.py
+++ ［mongoDB］Insert_Data.py
@@ -4,19 +4,23 @@ import json
 def ImportData(filename):
     # 讀取json檔案
     with open(filename,'r', encoding='utf-8') as data:
-    result_data = json.load(data)
+        result_data = json.load(data)
     
     # 建立資料庫連線
     client = pymongo.MongoClient("localhost", 27017)
     
-    db = client.ProductRawData
+    # 建立／選擇database
+    db = client.testDB
+    
+    # 建立／選擇collection
     collection = db.productInfo
     
+    # insert資料至collection
     collection.insert_many(result_data)
     
     client.close()
     
 if __name__ == "__main__":
     # json檔案位置
-    filename = "product_info_NY2"
-    ImportData()
+    filename = "product_info_NY2.json"
+    ImportData(filename)
